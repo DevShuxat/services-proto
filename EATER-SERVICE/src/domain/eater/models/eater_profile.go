625bd45f5014c3d3d1b6490932611a522bfe5ab8
DevShuxrat <tulaganovshuxratulugbek@gmail.com>
2023-09-05 10:43:16 +0500

    ++++

---
+++ EATER-SERVICE/src/domain/eater/models/eater_profile.go
@@ -0,0 +1,14 @@
+package models 
+
+import (
+	"time"
+)
+
+type EaterProfile struct {
+	EaterID string
+	Name string
+	PhoneNumber string
+	ImageUrl string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	}
