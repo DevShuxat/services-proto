d69d1e1d7ee2b57c6f60beb14ecabf72f3c5dd18
DevShuxrat <tulaganovshuxratulugbek@gmail.com>
2023-09-29 10:28:50 +0500

    fix

--- README.md
+++
@@ -1,2 +0,0 @@
-# eater-service
-microservice in golang
--- src/infrastructure/validator/phone_number.go
+++
@@ -1,9 +0,0 @@
-package validator
-
-import "regexp"
-
-func ValidateUzPhoneNumber(phoneNumber string) bool {
-	regex := `^998\d{9}$`
-	re := regexp.MustCompile(regex)
-	return re.MatchString(phoneNumber)
-}
